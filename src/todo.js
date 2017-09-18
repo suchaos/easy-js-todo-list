d0e98469d0b608981069c0b3cd68d7ccac66efb7
suchao <822075222@qq.com>
2017-09-18 15:51:51 +0800

    localStorage 存储 todoList

--- src/todo.js
+++ src/todo.js
@@ -2,26 +2,41 @@ var log = function() {
     console.log.apply(console, arguments)
 }
 
+var todoList = []
+
 var addButton = document.querySelector("#id-button-add")
 addButton.addEventListener("click", function() {
     var todoInput = document.querySelector("#id-input-todo")
-    var todo = todoInput.value
+    var task = todoInput.value
+    var todo = {
+        task: task,
+        time: currentTime()
+    }
+    todoList.push(todo)
+    saveTodoList()
+    insertTodoToHtml(todo)
+})
+
+function insertTodoToHtml(todo) {
+    var todoInput = document.querySelector("#id-input-todo")
+    var task = todoInput.value
     var todoContainer = document.querySelector(".todo-container")
     var template = templateTodo(todo)
-    if (todo) {
+    if (todo.task) {
         todoContainer.insertAdjacentHTML('beforeend', template)
         todoInput.value = ''
     } else {
-        alert("请填写内容")
+        // alert("请填写内容")
     }
-})
+}
 
 function templateTodo(todo) {
     var t = `
     <div class="todo-cell">
         <button class="todo-done">完成</button>
         <button class="todo-delete">删除</button>
-        <span contenteditable="true">${todo}</span>
+        <span contenteditable="true">${todo.task}</span>
+        <span>${todo.time}</span>
     </div>
     `
     return t
@@ -34,7 +49,10 @@ todoContainer.addEventListener("click", function(event) {
     if (target.classList.contains("todo-done")) {
         toggleClass(todoDiv, "done")
     } else if (target.classList.contains("todo-delete")) {
+        var index = indexOfElement(target.parentElement)
         todoDiv.remove()
+        todoList.splice(index, 1)
+        saveTodoList()
     }
 })
 
@@ -44,4 +62,44 @@ function toggleClass(element, className) {
     } else {
         element.classList.add("done")
     }
+}
+
+function currentTime() {
+    var d = new Date()
+    var month = d.getMonth() + 1
+    var date = d.getDate()
+    var hours = d.getHours()
+    var minutes = d.getMinutes()
+    var seconds = d.getSeconds()
+    var timeString = `${month}/${date} ${hours}:${minutes}:${seconds}`
+    return timeString
+}
+
+// 返回自己在父元素中的下标
+function indexOfElement(element) {
+    var parent = element.parentElement
+    console.log(parent)
+    for (var i = 0; i < parent.children.length; i++) {
+        var e = parent.children[i]
+        console.log(parent.children[i])
+        if (e === element) {
+            return i
+        }
+    }
+}
+
+function saveTodoList() {
+    var s = JSON.stringify(todoList)
+    localStorage.todoList = s
+}
+
+function loadTodoList() {
+    var s = localStorage.todoList
+    return JSON.parse(s)
+}
+
+todoList = loadTodoList()
+for (var i = 0; i < todoList.length; i++) {
+    var todo = todoList[i];
+    insertTodoToHtml(todo)
 }
